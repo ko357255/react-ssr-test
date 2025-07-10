ac2df33e7cbe2d7ee8b4c19d6487ca1db58bcc4b
ko357255 <mikanace@outlook.jp>
2025-07-11 01:09:23 +0900

    Expressによる静的HTMLの配信を設定

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
-import { Route, Routes } from 'react-router-dom';
+// import { Route, Routes } from 'react-router-dom';
 import './App.css';
-import MyProfile from '@/page/MyProfile';
+// import MyProfile from '@/page/MyProfile';
 import Profile from '@/page/Profile';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<MyProfile />} />
-      <Route path="/profile" element={<Profile />} />
-    </Routes>
+    <Profile />
+    // <Routes>
+    //   <Route path="/" element={<MyProfile />} />
+    //   <Route path="/profile" element={<Profile />} />
+    // </Routes>
   );
 }
 
