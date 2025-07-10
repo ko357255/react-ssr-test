564888a291ef77c14b67b084429d2fee86ca70c0
ko357255 <mikanace@outlook.jp>
2025-07-10 10:00:02 +0900

    フォーマット

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
-import './App.css'
+import './App.css';
 
 function App() {
-
   return (
-    <div className='App'>
-
+    <div className="App">
+      <div className="p-10">
+        <h1>Hellow world</h1>
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
