40fea91ae2e06762fa945191fcc64187d1a231a4
ko357255 <mikanace@outlook.jp>
2025-07-16 21:45:28 +0900

    ホーム、404ページを作成

---
+++ src/pages/Home.tsx
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <div>
+      <h1 className="mb-4 text-2xl font-bold">ホーム</h1>
+    </div>
+  );
+};
+
+export default Home;
--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,12 +1,8 @@
 const NotFound = () => {
   return (
-    <div className="flex min-h-screen w-screen items-center justify-center bg-gray-200 p-4">
-      <div className="main-content w-full max-w-2xl">
-        <div className="mb-6 overflow-hidden rounded-lg bg-white p-8 shadow-xl backdrop-blur-sm">
-          <h1 className="mb-4 text-2xl font-bold">Not Found.</h1>
-          <h1 className="mb-4 text-2xl font-bold">このページは存在しません</h1>
-        </div>
-      </div>
+    <div className="notfound">
+      <h1 className="mb-4 text-2xl font-bold">Not Found.</h1>
+      <h1 className="mb-4 text-2xl font-bold">このページは存在しません</h1>
     </div>
   );
 };
