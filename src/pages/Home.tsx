6a9935b53d234d99cafaa8e4fcffdf676642ad27
ko357255 <mikanace@outlook.jp>
2025-07-16 22:00:27 +0900

    些細なデザインの修正

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,6 +2,7 @@ const Home = () => {
   return (
     <div>
       <h1 className="mb-4 text-2xl font-bold">ホーム</h1>
+      <p className="mb-2">ホームページです</p>
     </div>
   );
 };
--- src/pages/MyProfile.tsx
+++ src/pages/MyProfile.tsx
@@ -6,11 +6,11 @@ const MyProfile = () => {
   return (
     <div>
       <h1 className="mb-4 text-2xl font-bold">詳細ページ</h1>
-      <p className="mb-2 text-gray-700">好きなもの</p>
-      <p className="mb-2 text-gray-700">動物: 猫</p>
-      <p className="mb-2 text-gray-700">料理: オムライス</p>
-      <p className="mb-2 text-gray-700">ゲーム: マインクラフト</p>
-      <p className="mb-2 text-gray-700">カウンター: {count}</p>
+      <p className="mb-2">好きなもの</p>
+      <p className="mb-2">動物: 猫</p>
+      <p className="mb-2">料理: オムライス</p>
+      <p className="mb-2">ゲーム: マインクラフト</p>
+      <p className="mb-2">カウンター: {count}</p>
       <button
         className="mb-2 rounded-2xl bg-black p-2 px-5 py-1 text-white shadow transition-colors hover:bg-gray-900 hover:shadow-md active:bg-gray-700 active:shadow-inner"
         onClick={() => setCount(count + 1)}
